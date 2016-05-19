01401cb9b0d6c4858386b3f8a77d8624840e5172
srgkun <nectoside@gmail.com>
2016-05-19 12:46:30 +0300

    "Changed setting.py, init.sh, changed data base from sqlite3 on mysql"

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+
     }
 }
 
--- init.sh
+++ init.sh
@@ -1,7 +1,16 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 
+pip django install==1.8
+
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 
 sudo /etc/init.d/nginx restart
+
+
+sudo /etc/init.d/mysql restart
+
+mysql -u root -e
+create database mydb;
+./manage.py migrate
